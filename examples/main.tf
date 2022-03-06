469484aa52eb59a34a8e7cf9bc2935ddf0d41585
tomarv2 <varuntomar2019@gmail.com>
2022-03-05 19:06:13 -0800

    initial commit

--- examples/main.tf
+++ examples/main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 terraform {
   required_version = ">= 1.0.1"
   required_providers {
@@ -16,19 +15,10 @@ module "acr" {
 
   resource_group_name = "demo-resource_group"
   # DOCKER BUILD
-  deploy_image      = true
+  deploy_image = true
   # Path to script directory relative to current location
   dockerfile_folder = "../scripts"
   location          = var.location
-=======
-module "acr" {
-  source = "../"
-
-  rg_name = "test-rg"
-  # DOCKER BUILD
-  deploy_image      = true
-  dockerfile_folder = "/scripts"
->>>>>>> 31e0ceeb7a3465e9b1567032ec2d4bb7467b4b98
   # WEBHOOK
   webhooks = [
     {
--- examples/variables.tf
+++ examples/variables.tf
@@ -1,20 +1,14 @@
 variable "teamid" {
   description = "Name of the team/group e.g. devops, dataengineering. Should not be changed after running 'tf apply'"
-<<<<<<< HEAD
   type        = string
-=======
->>>>>>> 31e0ceeb7a3465e9b1567032ec2d4bb7467b4b98
 }
 
 variable "prjid" {
   description = "Name of the project/stack e.g: mystack, nifieks, demoaci. Should not be changed after running 'tf apply'"
-<<<<<<< HEAD
   type        = string
 }
 variable "location" {
   description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created"
   type        = string
   default     = "westus2"
-=======
->>>>>>> 31e0ceeb7a3465e9b1567032ec2d4bb7467b4b98
 }
